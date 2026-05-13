f31fee30d2c8a2f324a107fba26d9bd2c0126913
NaoTomori0 <kyongfam0@gmail.com>
2026-05-13 18:00:40 +0300

    update

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from app.models import Post
 from datetime import datetime
+import os
 
 bp = Blueprint("main", __name__)
 
@@ -13,6 +14,12 @@ def index():
         .limit(30)
         .all()
     )
+    memorial_name = os.environ.get("MEMORY_NAME", "")
+    memorial_years = os.environ.get("MEMORY_YEARS", "")
     return render_template(
-        "index.html", flying_posts=flying_posts, year=datetime.now().year
+        "index.html",
+        flying_posts=flying_posts,
+        memorial_name=memorial_name,
+        memorial_years=memorial_years,
+        year=datetime.now().year,
     )
